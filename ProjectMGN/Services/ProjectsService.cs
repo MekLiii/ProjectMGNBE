1182fea1d176fcbb4d956aaf55d1d7a25f3f0334
Kamil Czacharowski <theczacharek@gmail.com>
2023-07-19 22:02:25 +0200

    Prepering for implementation of adding images

--- ProjectMGN/Services/ProjectsService.cs
+++ ProjectMGN/Services/ProjectsService.cs
@@ -11,14 +11,30 @@ namespace ProjectMGN.Services
         {
             _projectsRepository = projectsRepository;
         }
+        private bool IsBase64String(string base64string)
+        {
+            try
+            {
+                Convert.FromBase64String(base64string);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
         public void CreateProject(Projects projects, int OwnerId)
         {
+            if (projects.Image != null && IsBase64String(projects.Image))
+            {
+                throw new InvalidOperationException("Image is not base64 string");
+            }
             _projectsRepository.CreateProject(projects, OwnerId);
             return;
         }
         public List<Projects> GetAllProjects(int OwnerId)
         {
-           return _projectsRepository.GetAllProjects(OwnerId);
+            return _projectsRepository.GetAllProjects(OwnerId);
         }
     }
 }
