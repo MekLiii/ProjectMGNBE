6364bcdf1b79451af03716b73c90cc1056bc7f53
Kamil Czacharowski <theczacharek@gmail.com>
2023-07-26 21:37:43 +0200

    Made a global exception handler middleware

---
+++ ProjectMGN/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ProjectMGN.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            string message = JsonSerializer.Serialize(exception.Message);
+
+            await context.Response.WriteAsync(message);
+        }
+    }
+}
