f5adf8f308744d754c83d201eb61e66f85f77aa4
Kamil Czacharowski <theczacharek@gmail.com>
2023-08-08 21:11:39 +0200

    Added funcionality of ConfigurationService

--- ProjectMGN/Services/ConfigurationService.cs
+++ ProjectMGN/Services/ConfigurationService.cs
@@ -1,6 +1,35 @@
-﻿namespace ProjectMGN.Services
+﻿using ProjectMGN.Interfaces.Repositories;
+using ProjectMGN.Interfaces.Services;
+using ProjectMGN.Models;
+
+namespace ProjectMGN.Services
 {
-    public class ConfigurationService
+    public class ConfigurationService : IConfigurationService
     {
+        private readonly IProjectConfiguration _projectConfiguration;
+
+        public ConfigurationService(IProjectConfiguration projectConfiguration)
+        {
+
+            _projectConfiguration = projectConfiguration;
+        }
+
+        public void CreateConfiguration(Configuration configuration, int OwnerId)
+        {
+            _projectConfiguration.CreateConfiguration(configuration, OwnerId);
+        }
+        public List<Configuration> GetAllConfigurations(int OwnerId)
+        {
+            return _projectConfiguration.GetAllConfigurations(OwnerId);
+        }
+        public void DeleteConfiguration(int ownerId, int configurationId)
+        {
+            _projectConfiguration.DeleteConfiguration(ownerId, configurationId);
+        }
+        public Configuration GetConfigurationById(int ownerId, int configurationId)
+        {
+            return GetConfigurationById(ownerId, configurationId);
+        }
+
     }
 }
